4d13eea0269b78382ca7c7c9bc12d28a40d883ff
Hailey Bates <hailey.bates001@gmail.com>
2022-04-09 17:54:02 -0400

    set function for time on top of page to change every second

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 //moment.js for current date and time
-var currentDay = document.querySelector("#currentDay");
-
-const currentDay = moment().format("MMMM Do YYYY, h:mm:ss a");
+$("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+//sets clock to change every second on page
+let renderClock = function(){
+    document.querySelector("#currentDay").innerHTML =
+    `${moment().format('MMMM Do YYYY, h:mm:ss a')}`;
+};
+renderClock();
+setInterval(renderClock, 1000);

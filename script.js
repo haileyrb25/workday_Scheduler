df8454fcf84c828cafcbcee95e154e8e77b57b01
Hailey Bates <hailey.bates001@gmail.com>
2022-04-09 19:15:56 -0400

    still working on time blocks function

--- script.js
+++ script.js
@@ -9,5 +9,33 @@ renderClock();
 setInterval(renderClock, 1000);
 console.log()
 
+var infoText = document.querySelector("#info");
+var saveButtons = document.querySelector("#saveBtn");
+var blockHour = document.querySelector("#time-block");
 
 
+function trackHour() {
+var currentHour = moment().hour();
+
+$(".time-block").each(function() {
+    var blockHour = parseInt($(this).attr("id").split("hour")[0]);
+    console.log(blockHour, currentHour)
+
+    if (blockHour < currentHour) {
+        $(this).addClass("past");
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+        }
+    else if (blockHour === currentHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+        $(this).removeClass("future");
+        }
+    else {
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+        $(this).addClass("future");
+        }
+})
+trackHour();
+}

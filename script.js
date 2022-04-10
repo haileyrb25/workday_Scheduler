8817aedea45a5b280053346efad9ee3c62a2ea7b
Hailey Bates <hailey.bates001@gmail.com>
2022-04-09 22:06:17 -0400

    added functionality to save button from notes in class

--- script.js
+++ script.js
@@ -9,6 +9,18 @@ renderClock();
 setInterval(renderClock, 1000);
 console.log()
 
+//jquery syntax and save button
+$(document).ready(function() {
+$(".saveBtn").on("click", function() {
+    var value = $(this).sibling("info").val();
+    var time = $(this).parent().attr("id");
+localStorage.setItem(time, value)
+//this piece of code was givwn in hw startup- it let's us know what we are saving with save button
+})
+
+
+
+
 var infoText = document.querySelector("#info");
 var saveButtons = document.querySelector("#saveBtn");
 var blockHour = document.querySelector("#time-block");
